28f41e2b39beb0a7ecfea40c6ff74c6fa89ed85f
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-11-12 09:47:28 -0500

    change the indentation of the dictionnary

--- Programmation/pre-CQI-problem-set/hamming/test_decode.py
+++ Programmation/pre-CQI-problem-set/hamming/test_decode.py
@@ -3,8 +3,6 @@ import unittest
 from decode import Decoder
 
 class TestDecodeFunction(unittest.TestCase):
-   
-
     def test_separate_binary_from_base17(self):
         decoder = Decoder()
         result = decoder.get_binary_and_base17("0100000100101000:11d48ed9dCQIc6ab6c6147d845e586da03b9")
